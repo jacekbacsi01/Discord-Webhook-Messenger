b15e84af6f1fca19a2269579825b43dee07a3ed8
jacekbacsi01 <jacekbacsi01@users.noreply.github.com>
2020-08-14 17:42:23 +0200

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace COMDROID_DISCORD_WEBHOOK_MANAGER
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.textBoxMsg = new System.Windows.Forms.TextBox();
+            this.textBoxResp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.butAbout = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBoxCustomUsrNm = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxUsrName = new System.Windows.Forms.TextBox();
+            this.butSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(91, 12);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(295, 20);
+            this.textBoxURL.TabIndex = 0;
+            // 
+            // textBoxMsg
+            // 
+            this.textBoxMsg.Location = new System.Drawing.Point(91, 35);
+            this.textBoxMsg.Name = "textBoxMsg";
+            this.textBoxMsg.Size = new System.Drawing.Size(295, 20);
+            this.textBoxMsg.TabIndex = 2;
+            this.textBoxMsg.Text = "Helló Világ!";
+            // 
+            // textBoxResp
+            // 
+            this.textBoxResp.Location = new System.Drawing.Point(91, 61);
+            this.textBoxResp.Multiline = true;
+            this.textBoxResp.Name = "textBoxResp";
+            this.textBoxResp.ReadOnly = true;
+            this.textBoxResp.Size = new System.Drawing.Size(295, 41);
+            this.textBoxResp.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Üzenet:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "JSON Válasz:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(324, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Discord Webhook Messenger v0.2 by Alex Hegedűs(jacekbacsi01)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.butAbout);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.checkBoxCustomUsrNm);
+            this.groupBox1.Controls.Add(this.butSend);
+            this.groupBox1.Location = new System.Drawing.Point(392, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(310, 159);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opciók";
+            // 
+            // butAbout
+            // 
+            this.butAbout.Location = new System.Drawing.Point(279, 12);
+            this.butAbout.Name = "butAbout";
+            this.butAbout.Size = new System.Drawing.Size(25, 23);
+            this.butAbout.TabIndex = 11;
+            this.butAbout.Text = "?";
+            this.butAbout.UseVisualStyleBackColor = true;
+            this.butAbout.Click += new System.EventHandler(this.butAbout_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(303, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Discord logos and products, trademarks owned by Discord Inc.";
+            // 
+            // checkBoxCustomUsrNm
+            // 
+            this.checkBoxCustomUsrNm.AutoSize = true;
+            this.checkBoxCustomUsrNm.Location = new System.Drawing.Point(6, 16);
+            this.checkBoxCustomUsrNm.Name = "checkBoxCustomUsrNm";
+            this.checkBoxCustomUsrNm.Size = new System.Drawing.Size(257, 17);
+            this.checkBoxCustomUsrNm.TabIndex = 1;
+            this.checkBoxCustomUsrNm.Text = "Egyedi felhasználónév használata, URL mentése";
+            this.checkBoxCustomUsrNm.UseVisualStyleBackColor = true;
+            this.checkBoxCustomUsrNm.CheckedChanged += new System.EventHandler(this.checkBoxCustomUsrNm_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Felhasználónév:";
+            // 
+            // textBoxUsrName
+            // 
+            this.textBoxUsrName.Location = new System.Drawing.Point(91, 108);
+            this.textBoxUsrName.Name = "textBoxUsrName";
+            this.textBoxUsrName.ReadOnly = true;
+            this.textBoxUsrName.Size = new System.Drawing.Size(295, 20);
+            this.textBoxUsrName.TabIndex = 9;
+            // 
+            // butSend
+            // 
+            this.butSend.BackgroundImage = global::COMDROID_DISCORD_WEBHOOK_MANAGER_BY_ALEXHEGEDUS.Properties.Resources.discord_send_img;
+            this.butSend.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.butSend.Location = new System.Drawing.Point(6, 42);
+            this.butSend.Name = "butSend";
+            this.butSend.Size = new System.Drawing.Size(298, 98);
+            this.butSend.TabIndex = 1;
+            this.butSend.Text = "Küldés";
+            this.butSend.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.butSend.UseVisualStyleBackColor = true;
+            this.butSend.Click += new System.EventHandler(this.butSend_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 166);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxUsrName);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxResp);
+            this.Controls.Add(this.textBoxMsg);
+            this.Controls.Add(this.textBoxURL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Discord Webhook Messenger v0.2 by Alex Hegedűs";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button butSend;
+        private System.Windows.Forms.TextBox textBoxMsg;
+        private System.Windows.Forms.TextBox textBoxResp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxCustomUsrNm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxUsrName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button butAbout;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,209 @@
+﻿/*
+ * Made by: Alex Hegedus(jacekbacsi01) - 2020.01.16-17. -
+ * Felhasználtam: -A Discord Dev dokumentációt(kapcsolat felépítése, kommunikáció, használat), -Visual Studio dev dokumentációt(JSON, HTTP Kérések kezelése)
+ * Verziók:
+ * * V0.1 > -Alap dizájn, első működés, hibakódok kiírása
+ ***********
+ * * V0.2 > -Egyedi felhasználónév használata annak mentése fájlba, 
+ *           és köv. megnyitásnál újra betöltése -URL: szintén ugyan ez...
+ *          -Kivételek, hibakódok kezelése újraírva, kibővítve, sok helyzetben tesztelve!
+ *          -dizájn felturbózása egy picit :)
+ *          
+ * Megj.,Desc.: Használd egészséggel, ne írd át a készítő nevét. Kérlek tüntesd fel a készítő nevét ha tovább adod a programot!! :D
+ *              Ha észrevételed van, bátran jelezd nekem.
+ *              
+ *              Használat: Discordon belül szerveredre jobb klikk>Szerver Beállítások>Integrations>Webhooks létrehozol egyet és a linkjét az általam készített programba másolod
+ *                         a nevet tudod discordban is változtatni de akár távolról is.
+ */
+using System;
+using System.Collections.Generic;
+/*using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using System.Xml;
+
+namespace COMDROID_DISCORD_WEBHOOK_MANAGER
+{
+    public partial class Main : Form
+    {
+        int startenin = 0;
+        private HttpContent requestContent;
+
+        public Main()
+        {
+            InitializeComponent();
+            LoadUserSettings();
+        }
+        async void reqHTTP()
+        {
+            var client = new HttpClient();
+            
+            // Lekreáljuk a HTTP tartalmat(JSON-formában..)
+            if(checkBoxCustomUsrNm.Checked) //Ha be van pipálva akkor küldi az üzenetet, egyedi felhasználónevet
+            {
+                requestContent = new FormUrlEncodedContent(new[] {              
+                new KeyValuePair<string, string>("content", textBoxMsg.Text),
+                new KeyValuePair<string, string>("username", textBoxUsrName.Text)
+            }); ;
+            }
+            else //ha nincs akkor csak az üzenetet és azt a nevet fogja használni amit discord szervereden megadtál
+            {
+                requestContent = new FormUrlEncodedContent(new[] {
+                new KeyValuePair<string, string>("content", textBoxMsg.Text),   
+            });
+            }
+    
+            try
+            {
+                // Elküldjük a lekérést
+                HttpResponseMessage response = await client.PostAsync(textBoxURL.Text, requestContent);
+                
+                // Lekérjük a választ a szervertől
+                HttpContent responseContent = response.Content;
+
+                // Lekérjük az adatfolyamot
+                using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+                {
+                    // Kiírjuk a szerver választ; azokat átformázzuk és úgy firkáljuk ki a kedves usernak :)
+                    textBoxResp.Text = await reader.ReadToEndAsync();
+                    if (textBoxResp.Text.Contains("message"))
+                    {
+                        if (textBoxResp.Text.Contains("Invalid Webhook Token"))
+                        {
+                            MessageBox.Show("Nincs ilyen webhook token! Kérlek ellenőrizd..", "Discord Webhook MSSNGR v0.2 by Alex Hegedűs - HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        if(textBoxResp.Text.Contains("Cannot send an empty message"))
+                        {
+                            MessageBox.Show("Nem küldhetsz üres üzenetet..", "Discord Webhook MSSNGR v0.2 by Alex Hegedűs - HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        if (textBoxResp.Text.Contains("Unknown Webhook"))
+                        {
+                            MessageBox.Show("Hiba! Ismeretlen Webhook, ellenőrizd! \nFormátum: https://discordapp.com/api/webhooks/667424566204694529/dC0ubIrouP5pA6hdFli2d7wSIM2ZQvnId_kcu1s2STRz3SDBld1Z5kMkKxgZvxwsDPIe", "Discord Webhook MSSNGR v0.2 by Alex Hegedűs - HIBA", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                        }
+                        if(textBoxResp.Text.Contains("404: Not Found"))
+                        {
+                            MessageBox.Show("HTTP HIBA 404! Hiba, az oldal nem található, ellenőrizd a Webhook URL-t.", "Discord Webhook MSSNGR v0.2 by Alex Hegedűs - HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Üzenet nem lett elküldve! \nHibakód(JSON):" + textBoxResp.Text, "Discord Webhook MSSNGR v0.2 by Alex Hegedűs - HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Üzenet sikeresen elküldve a Discord szerverre. :)", "Discord Webhook MSSNGR v0.2 by Alex Hegedűs - Üzenet Elküldve!", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch(Exception eX) //Ha kivétel akad ne crasheljünk már ki :DD -=- if coming an exception we not crash :))
+            {
+                if (eX.ToString().Contains("URI"))
+                {
+                    MessageBox.Show("A megadott Webhook Link rossz formátumú.");
+                }
+                else
+                {
+                    if (eX.ToString().Contains("A távoli név nem oldható fel"))
+                    {
+                        MessageBox.Show("Nem sikerült csatlakozni a Discord kiszolgálójához, nem oldható fel a domain cím. Ellenőrizd az internetkapcsolatod!", "Discord Webhook MSSNGR v0.2 by Alex Hegedűs - HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    else if (eX.ToString().Contains("Nem lehet csatlakozni a távoli kiszolgálóhoz"))
+                    {
+                        MessageBox.Show("Nem sikerült csatlakozni a Discord kiszolgálójához, ellenőrizd az internetkapcsolatod!", "Discord Webhook MSSNGR v0.2 by Alex Hegedűs - HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                   
+                    else
+                    {
+                        MessageBox.Show(eX.ToString());
+                    }
+                }
+            }
+        }
+        void LoadUserSettings() //program mappájából tölti be az xml-ből a megjegyzett belépési adatokat...
+        {
+            XmlDocument docc = new XmlDocument(); //uj xml
+            if (File.Exists("settings.xml")) //Ha létezik a settings.xml fájl
+            {
+                docc.Load("settings.xml"); //beállítások betöltése
+                foreach (XmlNode node in docc.SelectNodes("//discord_mssngr")) 
+                {
+                    try
+                    {
+                        String usrnm = node.SelectSingleNode("username").InnerText;
+                        String url = node.SelectSingleNode("url").InnerText;
+                        String ischecked = node.SelectSingleNode("ticked").InnerText;
+                        textBoxURL.Text = url;
+                        textBoxUsrName.Text = usrnm;
+                        if (ischecked == "True")
+                        {
+                            checkBoxCustomUsrNm.Checked = true;
+                            //checkBoxSaveURL.Checked = true; 
+                        }
+                        else
+                        {
+                            checkBoxCustomUsrNm.Checked = false;
+                            //checkBoxSaveURL.Checked = false;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.ToString());
+                    }
+                }
+            }
+            else //Ha nem létezik a user-settings.xml fájl  -=- if file not exists: user-settings.xml
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml("<user><username>felhasznalonev</username><url>url cim</url><ticked>True</ticked><creator_keszito>Alex Hegedus</creator_keszito></user>");
+
+                doc.PreserveWhitespace = false;
+                doc.Save(@"settings.xml");
+                MessageBox.Show("Felhasználó Beállítások fájl-t nem találtam, ezért létrehoztam egy minta fájlt! Fájl neve: 'settings.xml', helye a program mappájában.");
+
+            }
+        }
+        void SaveSettings() //menti a megjegyezendő cuccokat
+        {
+            if (startenin == 0) //nem ment mikor elindul a program és beállítja a tickboxot amikor betöltjük a beállításokat, mert az is eventet generál ha a programbol allitod a tick state-et(bugfix) 
+                                //if program start setting up tick box and this generate an event for save settings but settings is not loaded before!
+            {
+                startenin++; //megnöveljük egyel a startenin változót, hogy tudjuk elindult a program és a tickboxot élesíthetjük
+                             //we know program started and activate saving
+            }
+            else
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml("<discord_mssngr><username>" + textBoxUsrName.Text + "</username><url>" + textBoxURL.Text + "</url><ticked>" + checkBoxCustomUsrNm.Checked.ToString() + "</ticked><creator_keszito>Alex Hegedus</creator_keszito></discord_mssngr>"); //XML tartalmát létrehozzuk
+
+                doc.PreserveWhitespace = false; 
+                doc.Save(@"settings.xml"); //Elmenti az beállítások xmlt  -=-  save settings xml              
+            }
+        }
+        private void butSend_Click(object sender, EventArgs e) //küldés gomb
+        {
+            reqHTTP(); //elindítjuk a http/json adatküldést&fogadást  -=-  start http/json data send/receive
+            SaveSettings(); //elmentjük a beállításokat -=- save settings
+        }
+
+        private void checkBoxCustomUsrNm_CheckedChanged(object sender, EventArgs e)
+        {
+            SaveSettings();
+            if (checkBoxCustomUsrNm.Checked)
+            {
+                textBoxUsrName.ReadOnly = false;
+            }
+        }
+
+        private void butAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Made by Alex Hegedűs(jacekbacsi01) GitHub: github.com/jacekbacsi01 Written in C# 2020.01.16. -A Hungarian COM_DROID Team Project.-");
+        }
+    }
+}
